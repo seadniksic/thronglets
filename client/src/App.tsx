7f983e5b0c18505e33b53b5fbd19bf525a5437ab
Sead Niksic <seaddniksic@gmail.com>
2025-05-12 18:54:45 -0400

    updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,51 @@
-import { useEffect, useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SimWorld from './sim_world.tsx'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { extend } from '@react-three/fiber'
+import { OrbitControls } from 'three-stdlib'
+extend({ OrbitControls })
+
+//import SimWorld from './sim_world.tsx'
 
 // App.tsx
 import { useSim } from './hooks/useSim';
 
 export default function App() {
-  const sim = useSim();
+return (
+    <div className="main_viewport">
 
-  useEffect(() => {
-    if (sim) {
-      console.log('Simulation loaded:', sim);
-      // sim._main(); or whatever entry point you use
-    }
-  }, [sim]);
-
-  return (
-    <div>
-      
-      <SimWorld />
+         <OrbitControls />
+        <Canvas>
+          <mesh>
+            <sphereGeometry args={[2, 32, 16]} />
+            <meshPhongMaterial />
+          </mesh>
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[0, 0, 5]} color="red" />
+        </Canvas>
     </div>
-  );
+    
+)
 }
+
+//export default function App() {
+  //const sim = useSim();
+
+  //useEffect(() => {
+    //if (sim) {
+      //console.log('Simulation loaded:', sim);
+      //// sim._main(); or whatever entry point you use
+    //}
+  //}, [sim]);
+
+  //return (
+    //<div>
+      
+      //<SimWorld />
+    //</div>
+  //);
+//}
 
 //export default App
