9f0db8780115a3da541e83fddb7e8024ab803dff
Sead Niksic <seaddniksic@gmail.com>
2025-05-14 22:53:03 -0400

    small changes

--- simulation/src/main.cpp
+++ simulation/src/main.cpp
@@ -11,13 +11,23 @@ static int tickCount = 0;
 EMSCRIPTEN_KEEPALIVE
 void start_simulation() {
     isRunning = true;
-    tickCount = 0;
 }
 
 EMSCRIPTEN_KEEPALIVE
 void stop_simulation() {
     isRunning = false;
     tickCount = 0;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void pause_simulation() {
+    isRunning = false;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void restart_simulation() {
+    isRunning = true;
+    tickCount = 1;
 }
 
 EMSCRIPTEN_KEEPALIVE
